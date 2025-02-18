f470a6f4f2f81f00bea88e504f602e854db9a081
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-18 00:26:44 +0000

    Changed a bit of config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,7 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# java
+set --export JAVA_HOME ~/.jdks/corretto-21.0.6
+set --export PATH $JAVA_HOME/bin $PATH
