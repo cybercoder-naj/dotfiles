859ff69938ad698de0a2277ffd96f8aac7e24fcb
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-19 18:14:23 +0000

    Set browser, add git alias and renamed waybar for vscode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# browser
+set --export BROWSER "zen-browser"
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
