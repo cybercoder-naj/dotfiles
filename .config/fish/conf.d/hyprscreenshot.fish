85c8864d1d2e30b4146f200de9ee10d15ed72fcf
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-03-17 22:22:25 +0000

    restarted broken fish config

--- .config/fish/conf.d/hyprscreenshot.fish
+++
@@ -1,3 +0,0 @@
-function hyprscreenshot -d "Takes a screenshot and annotates it";
-  hyprshot -m region --raw | satty --filename -
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# browser
-set --export BROWSER "zen-browser"
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# java
-set --export JAVA_HOME ~/.jdks/corretto-21.0.6
-set --export PATH $JAVA_HOME/bin $PATH
-
-# abbrs
-abbr -a rip rm -rfv
