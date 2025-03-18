4000da37deef091792af791e43d93fd9f5f50e1e
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-03-18 10:53:43 +0000

    add completions for bun

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
-  set fish_greeting
+  # Commands to run in interactive sessions can go here
 end
+
+# Disable greeting
+set -g fish_greeting ""
+
+set -gx EDITOR nvim
+set -gx BROWSER zen-browser
+set -gx BUN_INSTALL "$HOME/.bun"
+set -gx JAVA_HOME ~/.jdks/corretto-21.0.6
+
+fish_add_path -P "$BUN_INSTALL/bin" "$JAVA_HOME/bin"
