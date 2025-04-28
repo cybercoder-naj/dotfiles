7fcb9ef7ed7ce72462645566b53b694af50b542e
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-04-28 20:25:12 +0100

    waybar and fish changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,7 @@ set -gx BUN_INSTALL "$HOME/.bun"
 set -gx JAVA_HOME ~/.jdks/corretto-21.0.6
 
 # Zoxide
-zoxide init fish | source
+zoxide init --cmd cd fish | source
 
 # Path
-fish_add_path -P "$BUN_INSTALL/bin" "$JAVA_HOME/bin"
+fish_add_path -P "$BUN_INSTALL/bin" "$JAVA_HOME/bin" "/usr/local/go/bin"
