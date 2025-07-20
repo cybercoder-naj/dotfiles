efa13461b9d5f1cf68f0db24170d0a11f08a19d2
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-07-20 22:00:47 +0100

    added ui and nav plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,3 @@
-local load = function(mod)
-  package.loaded[mod] = nil
-  require(mod)
-end
-
-load("user.settings")
-load("user.keybindings")
+require("user.settings")
+require("user.keybindings")
 require("user.plugins")
-
--- .config/nvim/lua/user/keybindings.lua
+++ .config/nvim/lua/user/keybindings.lua
@@ -1,2 +1,5 @@
 vim.keymap.set('v', '<', '<gv');
 vim.keymap.set('v', '>', '>gv');
+
+vim.keymap.set('n', '<', '<<');
+vim.keymap.set('n', '>', '>>');
